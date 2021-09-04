eedc1eb25302ab39992d3793686b0bf9c83d3e1d
AliciaAraujoR <chrllyalicek@gmail.com>
2021-09-04 01:39:20 -0500

    build dev start

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import express from 'express'
 
 const app = express()
 
-app.listen(4000);
-console.log('Server listen on port', 4000)
+app.listen(3000);
+console.log('Server listen on port', 3000)
 //arranque la app
